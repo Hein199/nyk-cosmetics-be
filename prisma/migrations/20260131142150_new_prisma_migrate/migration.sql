aa878dae412389189e864871d2a3c1b8d7afea05
HeinNS <heinnaingsoe1999@gmail.com>
2026-01-31 21:27:23 +0700

    Update orders

---
+++ prisma/migrations/20260131142150_new_prisma_migrate/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Order" ADD COLUMN     "payment_type" "PaymentType",
+ADD COLUMN     "remark" TEXT;
--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,11 +1,24 @@
 import { Type } from 'class-transformer';
-import { ArrayMinSize, IsNotEmpty, IsString, ValidateNested } from 'class-validator';
+import { ArrayMinSize, IsEnum, IsISO8601, IsNotEmpty, IsOptional, IsString, ValidateNested } from 'class-validator';
+import { PaymentType } from '@prisma/client';
 import { CreateOrderItemDto } from './create-order-item.dto';
 
 export class CreateOrderDto {
   @IsString()
   @IsNotEmpty()
   customer_id: string;
+
+  @IsOptional()
+  @IsISO8601()
+  order_date?: string;
+
+  @IsOptional()
+  @IsEnum(PaymentType)
+  payment_type?: PaymentType;
+
+  @IsOptional()
+  @IsString()
+  remark?: string;
 
   @ValidateNested({ each: true })
   @Type(() => CreateOrderItemDto)
